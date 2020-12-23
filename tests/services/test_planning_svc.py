d8325edb27c7ed79c3b4f9aad5733ff49c269937
clittle <clittle@mitre.org>
2020-12-23 11:52:42 -0500

    Update tests

--- tests/services/test_planning_svc.py
+++ tests/services/test_planning_svc.py
@@ -56,6 +56,8 @@ def planner_stub(**kwargs):
 def setup_planning_test(loop, ability, agent, operation, data_svc, init_base_world):
     tability = ability(ability_id='123', executor='sh', platform='darwin', test=BaseWorld.encode_string('mkdir test'),
                        cleanup=BaseWorld.encode_string('rm -rf test'), variations=[])
+    sability = ability(ability_id='124', executor='sh', platform='darwin', test=BaseWorld.encode_string('whoami'),
+                       singleton=True, variations=[])
     tagent = agent(sleep_min=1, sleep_max=2, watchdog=0, executors=['sh'], platform='darwin')
     tsource = Source(id='123', name='test', facts=[], adjustments=[])
     toperation = operation(name='test1', agents=tagent, adversary=Adversary(name='test', description='test',
@@ -63,6 +65,7 @@ def setup_planning_test(loop, ability, agent, operation, data_svc, init_base_wor
                            source=tsource)
 
     loop.run_until_complete(data_svc.store(tability))
+    loop.run_until_complete(data_svc.store(sability))
 
     loop.run_until_complete(data_svc.store(
         Obfuscator(name='plain-text',
@@ -70,7 +73,7 @@ def setup_planning_test(loop, ability, agent, operation, data_svc, init_base_wor
                    module='plugins.stockpile.app.obfuscators.plain_text')
     ))
 
-    yield tability, tagent, toperation
+    yield tability, tagent, toperation, sability
 
 
 @pytest.fixture(params=stop_bucket_exhaustion_params)
@@ -78,7 +81,7 @@ def stop_bucket_exhaustion_setup(request, setup_planning_test):
     """
     Provides setup objects for tests of _stop_bucket_exhaustion().
     """
-    _, _, operation = setup_planning_test
+    _, _, operation, _ = setup_planning_test
     planner = planner_stub(stopping_condition_met=request.param['stopping_condition_met'])
     operation.state = operation.states[request.param["operation_state"]]
     return planner, operation, request.param['condition_stop'], request.param['assert_value']
@@ -89,7 +92,7 @@ class TestPlanningService:
     def test_add_ability_to_bucket(self, loop, setup_planning_test, planning_svc):
         b1 = 'salvador'
         b2 = 'hardin'
-        a, _, _ = setup_planning_test
+        a, _, _, _ = setup_planning_test
         loop.run_until_complete(planning_svc.add_ability_to_bucket(a, b1))
         assert a.buckets == [b1]
         loop.run_until_complete(planning_svc.add_ability_to_bucket(a, b2))
@@ -113,7 +116,7 @@ class TestPlanningService:
         assert loop.run_until_complete(planning_svc._stopping_condition_met(facts, stopping_condition)) is True
 
     def test_check_stopping_conditions(self, loop, fact, link, setup_planning_test, planning_svc):
-        ability, agent, operation = setup_planning_test
+        ability, agent, operation, _ = setup_planning_test
         operation.source.facts = []
         stopping_conditions = [fact(trait='s.o.f.', value='seldon')]
 
@@ -129,7 +132,7 @@ class TestPlanningService:
         assert loop.run_until_complete(planning_svc.check_stopping_conditions(stopping_conditions, operation)) is True
 
     def test_update_stopping_condition_met(self, loop, fact, link, setup_planning_test, planning_svc):
-        ability, agent, operation = setup_planning_test
+        ability, agent, operation, _ = setup_planning_test
         stopping_condition = fact(trait='t.c.t', value='boss')
 
         class PlannerStub():
@@ -149,7 +152,7 @@ class TestPlanningService:
         assert p.stopping_condition_met is True
 
     def test_sort_links(self, loop, link, planning_svc, setup_planning_test):
-        a, _, _ = setup_planning_test
+        a, _, _, _ = setup_planning_test
         l1 = link(command='m', paw='1', ability=a, score=1)
         l2 = link(command='a', paw='2', ability=a, score=2)
         l3 = link(command='l', paw='3', ability=a, score=3)
@@ -169,7 +172,7 @@ class TestPlanningService:
         """
         Case 1 - let planner run until it stops itself after bucket 'three'.
         """
-        ability, agent, operation = setup_planning_test
+        ability, agent, operation, _ = setup_planning_test
         p = PlannerFake(operation)
         loop.run_until_complete(planning_svc.execute_planner(p, publish_transitions=False))
         assert p.calls == ['one', 'two', 'three']
@@ -179,7 +182,7 @@ class TestPlanningService:
         Case 2 - start planner but then hijack operation after bucket 'two and flag that stopping condition
         been found, thus stopping the planner when it attempt to proceed to next bucket
         """
-        ability, agent, operation = setup_planning_test
+        ability, agent, operation, _ = setup_planning_test
 
         async def stub_update_stopping_condition_met(planner, operation):
             if planner.calls == ['one', 'two']:
@@ -194,7 +197,7 @@ class TestPlanningService:
         Case 3 - start planner but then hijack operation and set it to 'FINISH' state, thus
         stopping the planner when it attempts to proceed to next bucket
         """
-        ability, agent, operation = setup_planning_test
+        ability, agent, operation, _ = setup_planning_test
 
         async def stub_update_stopping_condition_met_1(planner, operation):
             if planner.calls == ['one']:
@@ -205,7 +208,7 @@ class TestPlanningService:
         assert p.calls == ['one']
 
     def test_get_cleanup_links(self, loop, setup_planning_test, planning_svc):
-        ability, agent, operation = setup_planning_test
+        ability, agent, operation, _ = setup_planning_test
         operation.add_link(Link.load(dict(command='', paw=agent.paw, ability=ability, status=0)))
         links = loop.run_until_complete(
             planning_svc.get_cleanup_links(operation=operation, agent=agent)
@@ -215,6 +218,32 @@ class TestPlanningService:
         assert link_list[0].command == ability.cleanup[0]
 
     def test_generate_and_trim_links(self, loop, setup_planning_test, planning_svc):
-        ability, agent, operation = setup_planning_test
+        ability, agent, operation, _ = setup_planning_test
         generated_links = loop.run_until_complete(planning_svc.generate_and_trim_links(agent, operation, [ability]))
         assert 1 == len(generated_links)
+
+    def test_duplicate_lateral_filter(self, loop, setup_planning_test, planning_svc, link, fact):
+        ability, agent, operation, sability = setup_planning_test
+
+        l0 = link(command='a0', paw='0', ability=ability)
+        l1 = link(command='a1', paw='0', ability=sability)
+        l2 = link(command='a0', paw='0', ability=ability)
+        l3 = link(command='a1', paw='0', ability=sability)
+
+        l0.status = l0.states['SUCCESS']
+        l1.status = l1.states['SUCCESS']
+
+        filtered = [x for x in [l0, l1, l2, l3] if x.ability.singleton]
+        assert 2 == len(filtered)
+
+        operation.chain = [l0, l1]
+
+        # test historical filtering
+        filt = loop.run_until_complete(planning_svc.remove_completed_links(operation, agent, [l2, l3]))
+        assert 1 == len(filt)
+
+        # test parallel filtering
+        flat_fil = planning_svc._cross_check_agents_for_duplicate_singletons([[l0, l1, l2, l3],
+                                                                              [l0, l1, l2, l3],
+                                                                              [l0, l1, l2, l3]])
+        assert 7 == len(flat_fil)
